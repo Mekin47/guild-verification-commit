00ebb14b75eb49eee9c96ee14fab58dcba37a46e
Markus <91991642+Mekin47@users.noreply.github.com>
2025-10-31 01:22:27 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T22:22:15.261Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-10-30T22:22:27.087Z
+Commit: Fix broken links in documentation
