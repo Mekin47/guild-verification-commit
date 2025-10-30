d341f1532a210cf5efcbbd9d190baa7cbd6ffae2
Markus <91991642+Mekin47@users.noreply.github.com>
2025-10-31 01:22:32 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T22:22:27.087Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-30T22:22:32.180Z
+Commit: Fix CSS styling bug on mobile devices
